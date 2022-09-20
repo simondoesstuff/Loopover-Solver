a1342e650a63bd798d5a4e3bbca780efad8dd800
simondoesstuff <sw.simonwalker@gmail.com>
2022-09-20 00:27:22 -0600

    massive changes to piece dragging in Board class naive piece ordering works

--- main.py
+++ main.py
@@ -22,81 +22,134 @@ class Board:
     
     def isSolved(self):
         return matrixListToStr(self.board) == matrixListToStr(self.targetBoard)
-    
-    
-    def idealPosition(self, y, x):
-        return self._targetPositionsMap[self.board[y][x]]
-    
-    
-    def apply(self, action, quantity=1):
+
+
+    def findPiece(self, piece):
         """
-        Apply an action to the board
-        :param quantity: amount of times an action should be applied
-        :param action: For example, "R0" or "U3" as in (row 0) shift right or (row 3) shift up.
+        Find the location of a piece in the board
+        :param piece:
+        :return:
+        """
+
+        for y in range(0, self.dimensions[0]):
+            for x in range(0, self.dimensions[1]):
+                if (self.board[y][x] == piece):
+                    return (y, x)
+
+
+    def idealPosition(self, piece):
+        """
+        Find the ideal location of a piece based on the target board.
+        :param piece: The target piece to find the ideal position of. List is in targetBoard
+        :return:
+        """
+
+        return self._targetPositionsMap[piece]
+    
+
+    def dragPiece(self, piece, movement):
+        """
+        Move the board by dragging a piece.
+        :param piece:
+        :param movement:
+        :return:
+        """
+
+        return self.dragLocation(self.findPiece(piece), movement)
+
+
+    def dragLocation(self, location, movement):
+        """
+        Move the board by dragging a particular location.
+        :param movement: (y, x) tuple of motion. One of y or x must be zero. Only one dimension at a time.
         """
         
-        # 0 of an action means do nothing
-        if quantity <= 0:
+        # 0 motion means do nothing
+        if movement == (0, 0):
             return
-            
-        self.steps.append(action)
-        
-        direction = action[0]
-        subject = int(action[1])
-        
-        # horizontal shifts
-        if direction == "L" or direction == "R":
-            row = self.board[subject]
-            if direction == "L":
-                # left shift
-                row.append(row.pop(0))
+
+        # we can only move in one direction at a time
+        if movement[0] != 0 and movement[1] != 0:
+            raise "Only one direction of motion allowed at a time."
+
+        direction = None
+        target = None
+        magnitude = None
+        if movement[0] == 0:
+            magnitude = movement[1]
+            if magnitude < 0:
+                direction = "L"
+                target = location[0]
             else:
-                # right shift
-                row.insert(0, row.pop())
-        # vertical shifts
+                direction = "R"
+                target = location[0]
         else:
-            if direction == "D":
-                # down shift
-                spare = self.board[-1][subject]
-                
-                for i in range(0, len(self.board) - 1):
-                    self.board[-i - 1][subject] = self.board[-i - 2][subject]
-                    
-                self.board[0][subject] = spare
+            magnitude = movement[0]
+            if magnitude < 0:
+                direction = "U"
+                target = location[1]
             else:
-                # up shift
-                spare = self.board[0][subject]
-                
-                for i in range(0, len(self.board) - 1):
-                    self.board[i][subject] = self.board[i + 1][subject]
-                
-                self.board[-1][subject] = spare
-        
-        # quantity means repeat an action
-        self.apply(action, quantity - 1)
+                direction = "D"
+                target = location[1]
+
+        for i in range(abs(magnitude)):
+            self.steps.append(direction + str(target))
+
+            # horizontal shifts
+            if direction == "L" or direction == "R":
+                row = self.board[target]
+                if direction == "L":
+                    # left shift
+                    row.append(row.pop(0))
+                else:
+                    # right shift
+                    row.insert(0, row.pop())
+            # vertical shifts
+            else:
+                if direction == "D":
+                    # down shift
+                    spare = self.board[-1][target]
+
+                    for i in range(0, len(self.board) - 1):
+                        self.board[-i - 1][target] = self.board[-i - 2][target]
+
+                    self.board[0][target] = spare
+                else:
+                    # up shift
+                    spare = self.board[0][target]
+
+                    for i in range(0, len(self.board) - 1):
+                        self.board[i][target] = self.board[i + 1][target]
+
+                    self.board[-1][target] = spare
 
 
+    def print(self):
+        print(matrixListToStr(self.board))
+        print()
+
+
+# noinspection SpellCheckingInspection
 def loopover(scrambledBoard, solvedBoard):
     board = Board(scrambledBoard, solvedBoard)
-    
-    for y in range(0, board.dimensions[0]):
-        for x in range(0, board.dimensions[1]):
-            idealPos = board.idealPosition(y, x)
-            
-            # skip pieces already in the correct location
-            if idealPos == (y, x):
+
+    for row in board.targetBoard:
+        for peice in row:
+            board.print()
+            idealPosition = board.idealPosition(peice)
+            currentPosition = board.findPiece(peice)
+
+            # this piece is supposed to be on the first layer
+            if idealPosition[0] == 0:
+                board.dragPiece(peice, (0, idealPosition[1] - currentPosition[1]))
+                board.dragPiece(peice, (idealPosition[0] - currentPosition[0], 0))
                 continue
-            
-            # very first piece is easy
-            if idealPos == (0, 0):
-                board.apply(f"L{y}", x - idealPos[1])
-                board.apply(f"U{x}", y - idealPos[0])
-    
-    
-    print(scrambledBoard)
-    print()
-    print(matrixListToStr(board.board))
-    print()
+
+            board.dragLocation((0, idealPosition[1]), (idealPosition[0] - currentPosition[0], 0))
+            board.dragPiece(peice, (0, idealPosition[1] - currentPosition[1]))
+            board.dragPiece(peice, (idealPosition[0] - currentPosition[0], 0))
+
+    board.print()
     print(board.steps)
     
     return board.steps
